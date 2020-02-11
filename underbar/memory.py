2b44e0a28ff588f6f2f593f76ffee6ce184c06ac
Mikael Lind <elemel@elemel.se>
2020-02-11 20:41:01 +0100

    Simplify memory indexing

--- underbar/memory.py
+++ underbar/memory.py
@@ -3,17 +3,17 @@ from fractions import Fraction as Q
 
 class Memory:
     def __init__(self, size):
-        self.arrays = [size * [Q(0)]]
+        self.arrays = [None, size * [Q(0)]]
         self.pool = []
 
     def __getitem__(self, key):
-        i = key.denominator - 1
+        i = key.denominator
         j = key.numerator // key.denominator
         assert j >= 0
         return self.arrays[i][j]
 
     def __setitem__(self, key, value):
-        i = key.denominator - 1
+        i = key.denominator
         j = key.numerator // key.denominator
         assert j >= 0
         self.arrays[i][j] = value
@@ -23,19 +23,19 @@ class Memory:
             key = self.pool.pop()
         else:
             self.arrays.append(None)
-            key = Q(1, len(self.arrays))
+            key = Q(1, len(self.arrays) - 1)
 
-        i = key.denominator - 1
+        i = key.denominator
         self.arrays[i] = size * [Q(0)]
         return key
 
     def delete(self, key):
-        i = key.denominator - 1
-        assert i >= 1 and key.numerator == 1
+        i = key.denominator
+        assert i >= 2 and key.numerator == 1
         assert self.arrays[i] is not None
         self.arrays[i] = None
 
-        if i == self.next_denominator - 2:
-            self.next_denominator -= 1
+        if i == len(self.arrays) - 1:
+            self.arrays.pop()
         else:
             self.pool.append(key)
