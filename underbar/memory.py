3ae70233e9fa41a59992d1d1dcf44de4d03fbc68
Mikael Lind <elemel@elemel.se>
2020-03-10 23:05:49 +0100

    Use all fractions as base addresses

--- underbar/memory.py
+++ underbar/memory.py
@@ -1,41 +1,45 @@
 from fractions import Fraction as Q
 
 
+def next_rational(q: Q) -> Q:
+    dq = Q(1, q.denominator)
+
+    while True:
+        q += dq
+
+        if q >= 1:
+            return Q(1, dq.denominator + 1)
+
+        if q.denominator == dq.denominator:
+            return q
+
+
 class Memory:
-    def __init__(self, size):
-        self.arrays = [None, size * [Q(0)]]
+    def __init__(self, size: int) -> None:
+        self.arrays = {Q(0): size * [Q(0)]}
         self.pool = []
+        self.next_key = Q(1, 2)
 
-    def __getitem__(self, key):
-        i = key.denominator
-        j = key.numerator // key.denominator
-        assert j >= 0
-        return self.arrays[i][j]
+    def __getitem__(self, address: Q) -> Q:
+        index, key = divmod(address, 1)
+        assert index >= 0
+        return self.arrays[key][index]
 
-    def __setitem__(self, key, value):
-        i = key.denominator
-        j = key.numerator // key.denominator
-        assert j >= 0
-        self.arrays[i][j] = value
+    def __setitem__(self, address: Q, value: Q) -> None:
+        index, key = divmod(address, 1)
+        assert index >= 0
+        self.arrays[key][index] = value
 
-    def new(self, size):
+    def new(self, size: int) -> Q:
         if self.pool:
             key = self.pool.pop()
         else:
-            self.arrays.append(None)
-            key = Q(1, len(self.arrays) - 1)
+            key = self.next_key
+            self.next_key = next_rational(self.next_key)
 
-        i = key.denominator
-        self.arrays[i] = size * [Q(0)]
+        self.arrays[key] = size * [Q(0)]
         return key
 
-    def delete(self, key):
-        i = key.denominator
-        assert i >= 2 and key.numerator == 1
-        assert self.arrays[i] is not None
-        self.arrays[i] = None
-
-        if i == len(self.arrays) - 1:
-            self.arrays.pop()
-        else:
-            self.pool.append(key)
+    def delete(self, key: Q) -> None:
+        del self.arrays[key]
+        self.pool.append(key)
