b323892f2e399bffe9a3b9d6d43fa8592f626e46
Mikael Lind <elemel@elemel.se>
2020-03-16 20:49:18 +0100

    Make memory a stack of stacks of stacks

--- quest/memory.py
+++ quest/memory.py
@@ -16,17 +16,17 @@ def next_rational(q: Q) -> Q:
 
 class Memory:
     def __init__(self) -> None:
-        self.stacks = {}
+        self.stacks = []
         self.pool = []
         self.next_base = Q(0)
 
     def __getitem__(self, address: Q) -> Q:
         offset, base = divmod(address, 1)
-        return self.stacks[base][offset]
+        return self.stacks[base.denominator][base.numerator][offset]
 
     def __setitem__(self, address: Q, value: Q) -> None:
         offset, base = divmod(address, 1)
-        self.stacks[base][offset] = value
+        self.stacks[base.denominator][base.numerator][offset] = value
 
     def new(self, size: int = 0) -> Q:
         if self.pool:
@@ -35,18 +35,25 @@ class Memory:
             base = self.next_base
             self.next_base = next_rational(self.next_base)
 
-        self.stacks[base] = size * [Q(0)]
+            while len(self.stacks) <= base.denominator:
+                self.stacks.append([])
+
+            while len(self.stacks[base.denominator]) <= base.numerator:
+                self.stacks[base.denominator].append(None)
+
+        self.stacks[base.denominator][base.numerator] = size * [Q(0)]
         return base
 
     def delete(self, base: Q) -> None:
-        del self.stacks[base]
-        self.pool.append(base)
+        if self.stacks[base.denominator][base.numerator] is not None:
+            self.stacks[base.denominator][base.numerator] = None
+            self.pool.append(base)
 
     def push(self, base: Q, value: Q) -> None:
-        self.stacks[base].append(value)
+        self.stacks[base.denominator][base.numerator].append(value)
 
     def pop(self, base: Q) -> Q:
-        return self.stacks[base].pop()
+        return self.stacks[base.denominator][base.numerator].pop()
 
     def size(self, base: Q) -> int:
-        return len(self.stacks[base])
+        return len(self.stacks[base.denominator][base.numerator])
